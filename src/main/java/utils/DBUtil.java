17c67ee7672a4c26a1a8e948af84f107ccf82943
y_hibasami <y_hibasami@DELL-062>
2024-05-15 11:52:49 +0900

    DBUtil作成

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -1,5 +1,23 @@
 package utils;
 
-public class DBUtil {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import constants.JpaConst;
+
+public class DBUtil {
+    private static EntityManagerFactory emf;
+    
+    public static EntityManager creatEntityManager() {
+        return _getEntityManagerFactory().createEntityManager(); 
+    }
+    
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTENCE_UNIT_NAME);
+        }
+
+        return emf;
+    }
 }
